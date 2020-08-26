4d7005b639e248cec4d89e4bb792f6c291933012
annakuskova <55314661+annakuskova@users.noreply.github.com>
2020-08-26 19:28:49 +0300

    Update Вывод изображения.py

--- Вывод изображения.py
+++ Вывод изображения.py
@@ -1,29 +1,28 @@
-# example of inference with a pre-trained coco model
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from mrcnn.visualize import display_instances
 from mrcnn.config import Config
 from mrcnn.model import MaskRCNN
 
-# define 81 classes that the coco model knowns about
-class_names = ['kangaroo', 'bmw','car', 'toyota', 'peugeot']
+
+class_names = ['BG', 'bus', 'truck', 'car', 'bmw', 'toyota', 'peugeot', 'kia', 'zonda', 'man', 'hyundai', 'scania’']
 
 # define the test configuration
 class TestConfig(Config):
      NAME = "test"
      GPU_COUNT = 1
      IMAGES_PER_GPU = 1
-     NUM_CLASSES = 1 + 5
+     NUM_CLASSES = 1 + 11
 
 # define the model
 rcnn = MaskRCNN(mode='inference', model_dir='./', config=TestConfig())
 # load coco model weights
 rcnn.load_weights('mask_rcnn_coco.h5', by_name=True)
 # load photograph
-img = load_img('elephant.jpg')
+img = load_img('2.jpg')
 img = img_to_array(img)
 # make prediction
-results = rcnn.detect([img], verbose=0)
+results = rcnn.detect([img], verbose=1)
 # get dictionary for first prediction
 r = results[0]
 # show photo with bounding boxes, masks, class labels and scores
